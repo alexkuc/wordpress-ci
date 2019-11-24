d28822bd78d0d26103026d3c596d9b1d078e035c
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2019-11-24 06:07:04 +0200

    Add WordPress plugin “My Plugin”

---
+++ my-plugin/my-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     My Plugin
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     My Custom Plugin
+ * Author:          Alexander Kucheryuk
+ * Author URI:      https://github.com/alexkuc
+ * Text Domain:     my-plugin
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         My_Plugin
+ */
+
+// Your code starts here.
