5f5f50cdbd033759df2b4dfba9a67b9af7d9aae9
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2019-11-25 20:09:35 +0200

    Add CircleCI config

---
+++ .circleci/fix-permissions.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# credit goes to Van Eyck
+# see https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -Eeuo pipefail
+trap 'printf "\n[ERROR]: Error occurred at $BASH_SOURCE:$LINENO\n[COMMAND]: $BASH_COMMAND\n"' ERR
+
+sudo chmod -R 777 .
