1ec7b68c279b2021052c2a59be63656929e9e21c
Alexander Kucheryuk <3331946+alexkuc@users.noreply.github.com>
2020-01-14 05:41:21 +0200

    Add jshint, grunt clean and create assets locally

--- scripts/lint/jshint.sh
+++ scripts/lint/jshint.sh
@@ -5,13 +5,7 @@
 set -Eeuo pipefail
 trap 'printf "\n[ERROR]: Error occurred at $BASH_SOURCE:$LINENO\n[COMMAND]: $BASH_COMMAND\n"' ERR
 
-case "${1:-}" in
-    shellcheck | phplint | phpcs | jshint )
-        ./scripts/lint/"$1".sh
-        ;;
-    *)
-        for i in scripts/lint/[^lint]*.sh; do
-            bash "$i"
-        done
-        ;;
-esac
+(
+    cd src/
+    yarn grunt jshint:prod -vvv
+)
